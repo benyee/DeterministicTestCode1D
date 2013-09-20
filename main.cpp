5d597f0b47e4950e35bccf4375ecc97e08e574f7
Ben <benyee@berkeley.edu>
2013-09-20 01:32:35 -0400

    Hello world!

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  
+//
+//  Created by Ben Yee on 9/20/13.
+//
+//
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World!"<<endl;
+    return 0;
+}
