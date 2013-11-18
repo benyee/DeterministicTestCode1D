908d6549ce41cd7738818bb156be25b83a29e9ac
Ben <benyee@berkeley.edu>
2013-11-17 23:55:28 -0500

    Preparing for multiple inputs

--- main.cpp
+++ main.cpp
@@ -19,6 +19,26 @@ int main ()
 {
     cout << "Hello world!"<<endl;
     
+    //Generate input files:
+    static const double X_arr[] = {5,20,40};
+    vector<double> X(X_arr,X_arr+sizeof(X_arr)/sizeof(X_arr[0]));
+    static const double SigS_arr[] = {0.5,0.9,0.99,0.999,1};
+    vector<double> SigS(SigS_arr,SigS_arr+sizeof(SigS_arr)/sizeof(SigS_arr[0]));
+    static const double dx_arr[] = {2,0.5,0.125,0.01};
+    vector<double> dx(dx_arr,dx_arr+sizeof(dx_arr)/sizeof(dx_arr[0]));
+    static const double N_arr[] = {4};
+    vector<double> N(N_arr,N_arr+sizeof(N_arr)/sizeof(N_arr[0]));
+    
+    for(unsigned int i = 0; i<X.size();i++){
+        for(unsigned int j = 0; j<dx.size();j++){
+            for(unsigned int k = 0; k<SigS.size();k++){
+                
+            }
+        }
+    }
+    
+    
+    
     //Read in input:
     InputDeck *input = new InputDeck();
     int debug =input->loadInputDeck();
