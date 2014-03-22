c757345aacde1933ac31ee8cb182967def70b63f
Ben Yee <bcyee@umich.edu>
2014-03-22 17:10:03 -0400

    Added function to read in data from a file

--- python/data_read.py
+++ python/data_read.py
@@ -1,5 +1,33 @@
-import numpy
-from numpy import exp, sqrt, log, arange, array
 
-def data_read(path):
-    return 0
+def data_read(path,startline = 1):
+    print "Reading "+path
+    x = []
+    phi = []
+    with open(path,'r') as f:
+        foundData = 0;
+        foundData2 = 0;
+        
+        i = 0
+        for line in f:# and f.readline().contains('<'):
+            #Find out where to start:
+            i = i+1
+            if(i < startline):
+                continue
+    
+                    
+            if foundData2:
+                try:
+                    temp = line.split();
+                    x.append(temp[0]);
+                    phi.append(temp[1]);
+                except:
+                    print "Reached end of data"
+                    break;
+                    
+            if foundData:
+                foundData2 = 1
+            
+            if line[0]=='<':
+                foundData = 1
+
+    return x,phi
--- python/unit_tests.py
+++ python/unit_tests.py
@@ -1,13 +1,23 @@
 from data_read import data_read
 from numpy import array
+import math
+
+X_DATA = []
+PHI_DATA = []
 
 X = 20
-dx = (4,0.01,)
-eps = (0.1,1,)
-alpha = ("DD","SC","FV")
+dx = (4.,0.01,)
+eps = (0.1,1.,)
+alpha = ("DD","SC","new")
     
 for j in range(0,len(dx)):
     for m in range(0,len(alpha)):
         for k in range(0,len(eps)):
+            path = "../OutputFiles/Run11/output_11_"+alpha[m]+"_"+str(j)+"_"+str(j)+"_0_.txt"
             
-            print dx[j],alpha[m],eps[k]
+            #Calculate where we should start looking:
+            num_lines = sum(1 for line in open(path,'r'))
+            num_xvals = math.ceil(X/dx[j]*2)
+            startline = max(1,num_lines - num_xvals*2-30)
+            
+            data_read(path,int(startline))
