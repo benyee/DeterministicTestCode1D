76a66149eafb0be77f46599db61019169fbc3a20
Ben Yee <bcyee@umich.edu>
2014-04-03 01:07:31 -0400

    Added two new analysis scripts to python folder.

---
+++ python/Run13Analysis.py
@@ -0,0 +1,14 @@
+from data_set import data_set
+from numpy import array,cosh,sinh
+import math
+
+def diff_sol(x):
+    return 3.0 * ( 400 - x * x ) / 2 + 40
+
+X = 20
+eps = (0.03,0.1,0.25,0.5,1,)
+dx = (4,2,1,0.5,0.25,0.01,)
+alpha = ("new","SC")
+
+data_list = data_set(X,dx,alpha,eps,path_base="../OutputFiles/Run13/output_soln_13_",dx_eps_scale = 0)
+data_list.plot("../OutputFiles/Run13/",show = 0,fun=diff_sol,funlabel="Diff. Soln.")
